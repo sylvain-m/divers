2533b733c6e87dd43b7b7a0d8b6ba5d65e2691ab
Sylvain M. <montagnersylvain@yahoo.fr>
2026-04-24 12:02:01 +0200

    Create get_drone_uav_from_panoramax_parquet.py

---
+++ osm/panoramax/get_drone_uav_from_panoramax_parquet.py
@@ -0,0 +1,15 @@
+import duckdb
+
+conn = duckdb.connect()
+conn.execute("""
+    INSTALL spatial;
+    LOAD spatial;
+    COPY (
+        SELECT *
+        FROM 'https://api.panoramax.xyz/data/geoparquet/panoramax.parquet'
+        WHERE length(list_filter(semantics, s ->
+            s.key = 'transport' AND
+            (LOWER(s.value) = 'uav' OR LOWER(s.value) = 'drone')
+        )) > 0
+    ) TO 'sequences_uav_drone.geojson' WITH (FORMAT GDAL, DRIVER 'GeoJSON');
+""")
